1bd5995a952ce846a9e584b2a4f787b517f03c8c
Touhid <touhid@Touhids-MacBook-Pro.local>
2017-11-13 22:43:49 +0600

    update

--- app/src/main/java/com/touhidapps/androdiintentexamples/MainActivity.java
+++ app/src/main/java/com/touhidapps/androdiintentexamples/MainActivity.java
@@ -2,6 +2,7 @@ package com.touhidapps.androdiintentexamples;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,14 +28,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.buttonImplicitIntent:
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://google.com"));
-                startActivity(intent);
+
+                // To open url on browser
+//                Intent intent = new Intent(Intent.ACTION_VIEW);
+//                intent.setData(Uri.parse("https://touhidapps.com"));
+//                startActivity(intent);
+
+                // To make a phone call
+//                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:123"));
+//                startActivity(intent);
+
+                // To make a message
+//                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:123"));
+//                intent.putExtra("sms_body", "This is my message");
+//                startActivity(intent);
+
+                // To open android device settings
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+
                 break;
 
             case R.id.buttonExplicitIntent:
-                Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+
+                Intent i = new Intent(this, Main2Activity.class);
+                i.putExtra("NAME", "Touhid");
                 startActivity(i);
+
                 break;
         }
     }
